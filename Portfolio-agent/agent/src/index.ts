a6bcae9bffd587dd951dcba7b18ba8f9dc4bb398
natsuki <natsuki.ss@icloud.com>
2025-07-17 23:44:32 +0900

    Qiitaサブエージェントの作成

--- Portfolio-agent/agent/src/index.ts
+++ Portfolio-agent/agent/src/index.ts
@@ -4,55 +4,12 @@ import { GoogleGenAIProvider } from "@voltagent/google-ai";
 
 import { openai } from "@ai-sdk/openai";
 import { z } from "zod";
+import { getQiitaUserInfo, getQiitaUserItems } from "./tools/qiita.js";
 
-const getQiitaUserInfo = createTool({
-  name: "getQiitaUserInfo", // ツールの一意識別子。エージェントはこの名前でツールを呼び出す
-  description: "Qiitaユーザーの情報を取得する",
-  parameters: z.object({
-    userId: z.string().describe("QiitaユーザーID"),
-  }),
-  execute: async ({ userId }) => {
-    const accessToken = process.env.QIITA_API_KEY;
-    const response = await fetch(`https://qiita.com/api/v2/users/${userId}`, {
-      headers: {
-        Authorization: `Bearer ${accessToken}`,
-      },
-    });
-    const data = await response.json();
-    return data;
-  }, // 実際の処理を行う非同期関数
-});
-
-const getQiitaUserItems = createTool({
-  name: "getQiitaUserItems",
-  description: "QiitaユーザーIDを指定して、そのユーザーの投稿記事一覧を取得します。最大30件まで取得可能です。記事のタイトル、URL、いいね数、ストック数、閲覧数、タグ、作成日を返します。",
-  parameters: z.object({
-    userId: z.string().describe("記事一覧を取得したいQiitaユーザーのID"),
-  }),
-  execute: async ({ userId }) => {
-    const accessToken = process.env.QIITA_API_KEY;
-    const response = await fetch(`https://qiita.com/api/v2/users/${userId}/items?per_page=1`, {
-      headers: {
-        Authorization: `Bearer ${accessToken}`,
-      },
-    });
-
-    const data = await response.json();
-
-    return data.map((item: any) => ({
-      title: item.title,
-      url: item.url,
-      likes: item.likes_count,
-      stocks: item.stocks_count,
-      views: item.page_views_count,
-    }));
-  },
-});
-
-// Qiitaエージェントの作成
-const agent = new Agent({
+// Qiitaサブエージェントの作成
+const qiitaAgent = new Agent({
   name: "qiita-agent", // エージェントの識別名
-  instructions: "ユーザーからQiitaユーザーIDを受け取ったら、ユーザーの情報を取得してください。", // エージェントの動作指示
+  instructions: `ユーザーからQiitaユーザーIDを受け取ったら、ユーザーの情報を取得してください。`, // エージェントの動作指示
   parameters: z.object({
     userId: z.string().describe("QiitaユーザーID"),
   }), // 入力パラメータのスキーマ定義
@@ -66,8 +23,22 @@ const agent = new Agent({
   ]
 });
 
+const mainAgent = new Agent({
+  name: "main-agent",
+  instructions: `ユーザーから「QiitaユーザーID」を受け取ったら「QiitaユーザーID」を渡して情報をJSONで取得し、Qiitaのユーザー情報と投稿記事一覧をまとめて返してください。`,
+  parameters: z.object({
+    userId: z.string().describe("QiitaユーザーID"),
+  }),
+  subAgents: [
+    qiitaAgent,
+  ],
+  llm: new VercelAIProvider(),
+  model: openai("gpt-4o-mini"),
+});
+
+// 最初に呼び出すエージェント
 new VoltAgent({
   agents: {
-    agent,
+    mainAgent,
   },
 });
