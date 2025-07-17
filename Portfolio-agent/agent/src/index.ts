9439829b4299b101396bf955bfeea0f8ca274092
natsuki <natsuki.ss@icloud.com>
2025-07-17 23:55:24 +0900

    GitHubサブエージェントの作成

--- Portfolio-agent/agent/src/index.ts
+++ Portfolio-agent/agent/src/index.ts
@@ -5,6 +5,7 @@ import { GoogleGenAIProvider } from "@voltagent/google-ai";
 import { openai } from "@ai-sdk/openai";
 import { z } from "zod";
 import { getQiitaUserInfo, getQiitaUserItems } from "./tools/qiita.js";
+import { getGithubUserInfo, getGithubRepos } from "./tools/github.js";
 
 // Qiitaサブエージェントの作成
 const qiitaAgent = new Agent({
@@ -23,17 +24,39 @@ const qiitaAgent = new Agent({
   ]
 });
 
+// GitHubサブエージェントの作成
+const githubAgent = new Agent({
+  name: "github-agent",
+  instructions: `GitHubのユーザー名を指定して、GitHubのユーザー情報と公開リポジトリ数を取得してください。`,
+  parameters: z.object({
+    username: z.string().describe("GitHubのユーザーID"),
+  }),
+  // llm: new VercelAIProvider(),
+  // model: openai("gpt-4o-mini"),
+  llm: new GoogleGenAIProvider({ apiKey: process.env.GEMINI_API_KEY }),
+  model: "gemini-2.5-pro",
+  tools: [
+    getGithubUserInfo,
+    getGithubRepos],
+});
+
 const mainAgent = new Agent({
   name: "main-agent",
-  instructions: `ユーザーから「QiitaユーザーID」を受け取ったら「QiitaユーザーID」を渡して情報をJSONで取得し、Qiitaのユーザー情報と投稿記事一覧をまとめて返してください。`,
+  instructions: `
+    QiitaユーザーIDとGitHubユーザー名が与えられた場合は、絶対に確認や許可を求めたりせず、即座に両方の情報を取得し、Qiitaのユーザー情報・投稿記事一覧とGitHubのユーザー情報・公開リポジトリ情報をまとめて返してください。
+    サブエージェントやツールを呼び出す際にも、確認や同意のプロンプトは一切表示せず、ユーザーの追加アクションを求めないでください。`,
   parameters: z.object({
     userId: z.string().describe("QiitaユーザーID"),
+    username: z.string().describe("GitHubのユーザー名"),
   }),
   subAgents: [
     qiitaAgent,
+    githubAgent,
   ],
-  llm: new VercelAIProvider(),
-  model: openai("gpt-4o-mini"),
+  // llm: new VercelAIProvider(),
+  // model: openai("gpt-4o-mini"),
+  llm: new GoogleGenAIProvider({ apiKey: process.env.GEMINI_API_KEY }),
+  model: "gemini-2.5-pro",
 });
 
 // 最初に呼び出すエージェント
