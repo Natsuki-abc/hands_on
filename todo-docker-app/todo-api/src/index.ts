a9a20fda9cd18dcd7e8796af962e99eac202210a
natsuki <natsuki.ss@icloud.com>
2025-11-16 19:14:22 +0900

    API実装

--- todo-docker-app/todo-api/src/index.ts
+++ todo-docker-app/todo-api/src/index.ts
@@ -1,15 +1,55 @@
-import { serve } from '@hono/node-server'
-import { Hono } from 'hono'
+import { serve } from "@hono/node-server";
+import { Hono } from "hono";
 
-const app = new Hono()
+interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+}
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+const todos: Todo[] = [];
 
-serve({
-  fetch: app.fetch,
-  port: 3000
-}, (info) => {
-  console.log(`Server is running on http://localhost:${info.port}`)
-})
+const app = new Hono();
+
+app.get("/", (c) => {
+  return c.text("Hello Hono!");
+});
+
+// タスク一覧取得
+app.get("/todos", (c) => {
+  return c.json({ todos });
+});
+
+// タスクの追加
+app.post("/todos", async (c) => {
+  const { title } = await c.req.json();
+  const todo: Todo = {
+    id: todos.length + 1,
+    title,
+    completed: false,
+  };
+  todos.push(todo);
+  return c.json({ todo });
+});
+
+// タスク 完了/未完了 の更新
+app.put("/todos/:id", async (c) => {
+  const { id } = c.req.param();
+  const { completed } = await c.req.json();
+  const todo = todos.find((todo) => todo.id === Number(id));
+  if (!todo) {
+    return c.notFound(); // 404返却
+  }
+  todo.completed = completed;
+  return c.json({ todo });
+});
+
+serve(
+  {
+    fetch: app.fetch,
+    port: 3000,
+  },
+  (info) => {
+    console.log(`Server is running on http://localhost:${info.port}`);
+  }
+);
